0a021612dcf0e5c1aef9f487f078c298fdc99d1f
Hannes Wallnöfer <hannesw@gmail.com>
2010-11-17 15:30:11 +0100

    Port ringo basicauth middleware.

--- demo.js
+++ demo.js
@@ -1,17 +1,29 @@
 var {Server} = require("ringo/httpserver");
 var {Application} = require("stick");
-var {error, notfound, mount} = require("stick/middleware");
+var {error, notfound, mount, basicauth} = require("stick/middleware");
+
+// helper for creating simple dummy pages
+function dummyPage(text) {
+    return function(req) {
+        return { status: 200, headers: {}, body: [text] };
+    }
+}
 
 var app = new Application();
 app.configure(error, notfound, mount);
-app.mount("/hello", function(req) {
-    return {
-        status: 200, headers: {}, body: ["hello world!"]
-    };
-});
+
+app.mount("/hello", dummyPage("hello world!"));
 app.mount("/notfound", function(req) {
     throw { notfound: true };
 });
+
+// create a password protected admin application
+var admin = new Application(dummyPage("admin zone"));
+admin.configure(basicauth);
+// add basic authentication, password is "secret"
+admin.basicauth("/admin", "admin", "e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4");
+// mount on /admin
+app.mount("/admin", admin);
 
 var server = new Server({app: app});
 server.start();
