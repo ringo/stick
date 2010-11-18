fab11e1d4b349a3e5f0a2e8e30a69421cef65c3e
Hannes Wallnöfer <hannesw@gmail.com>
2010-11-18 15:06:29 +0100

    Reduce comments a bit

--- demo.js
+++ demo.js
@@ -15,26 +15,19 @@ function dummyPage(text) {
     }
 }
 
-// create a new blank Stick application
+// Our main application
 var app = exports.app = new Application();
-// configure notfound, mount, and static middleware
 app.configure(notfound, mount, static);
-// middleware chain now is notfound > mount > static > unhandled
-// mount hello world application at /hello
 app.mount("/hello", dummyPage("hello world!"));
-// throw to test error middleware, which is configured differently
-// in development and production environments
 app.mount("/error", function(req) {
     throw new Error("Something went wrong");
 });
-// serve files in lib as static resources 
-app.static(module.resolve("lib"));
+app.static(module.resolve("lib")); // serve files in lib as static resources
 
 // RINGO_ENV=production environment
 var prod = app.env("production");
 prod.configure(gzip, etag, error);
-// disable error location and stack traces for production env
-prod.error.location = false;
+prod.error.location = false; // disable error location and stack traces
 
 // RINGO_ENV=development environment
 var dev = app.env("development");
@@ -45,7 +38,6 @@ var admin = new Application(dummyPage("admin zone"));
 admin.configure(basicauth);
 // add basic authentication, password is "secret"
 admin.basicauth("/", "admin", "e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4");
-// mount on /admin
 app.mount("/admin", admin);
 
 // start server if we didn't already do so
