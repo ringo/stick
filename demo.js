93e87218ad1ced77e06e3c5826e56a6fb34de696
Hannes Wallnöfer <hannesw@gmail.com>
2010-11-18 15:03:41 +0100

    Port responselog middleware from Ringo, add environments to demo.js

--- demo.js
+++ demo.js
@@ -1,30 +1,44 @@
 var {Application} = require("stick");
 var {Server} = require("ringo/httpserver");
 var {Buffer} = require("ringo/buffer");
-var {gzip, etag, error, notfound, mount, static, basicauth} = require("stick/middleware");
+var log = require("ringo/logging").getLogger("demo");
+var {gzip, etag, error, notfound, mount, static, basicauth, responselog} =
+        require("stick/middleware");
 
 // helper for creating simple dummy pages
 function dummyPage(text) {
     return function(req) {
+        log.info(text);
         return { status: 200,
-                 headers: {"Content-Type": "text/plain"},
-                 body: new Buffer(text) };
+                 headers: {"Content-Type": "text/html"},
+                 body: new Buffer("<html><body>", text, "</body></html>") };
     }
 }
 
 // create a new blank Stick application
 var app = exports.app = new Application();
-// configure gzip, error, notfound, mount, and static middleware
-app.configure(gzip, etag, error, notfound, mount, static);
-// middleware chain now is gzip > error > notfound > mount > static > unhandled
+// configure notfound, mount, and static middleware
+app.configure(notfound, mount, static);
+// middleware chain now is notfound > mount > static > unhandled
 // mount hello world application at /hello
 app.mount("/hello", dummyPage("hello world!"));
-// throw notfound object to test notfound middleware
-app.mount("/notfound", function(req) {
-    throw { notfound: true };
+// throw to test error middleware, which is configured differently
+// in development and production environments
+app.mount("/error", function(req) {
+    throw new Error("Something went wrong");
 });
 // serve files in lib as static resources 
 app.static(module.resolve("lib"));
+
+// RINGO_ENV=production environment
+var prod = app.env("production");
+prod.configure(gzip, etag, error);
+// disable error location and stack traces for production env
+prod.error.location = false;
+
+// RINGO_ENV=development environment
+var dev = app.env("development");
+dev.configure(responselog, error);
 
 // create a password protected admin application
 var admin = new Application(dummyPage("admin zone"));
