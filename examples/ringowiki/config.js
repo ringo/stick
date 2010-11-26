b64f471a2c7d156535af457ba11d4af5daf4a2a2
Hannes Wallnöfer <hannesw@gmail.com>
2010-11-27 00:42:10 +0100

    Implement mount lookup via shared index object

--- examples/ringowiki/config.js
+++ examples/ringowiki/config.js
@@ -1,10 +1,9 @@
 
-var {Application} = require("stick");
 var {Store} = require('ringo/storage/filestore');
 
 exports.store = new Store('db');
 
-var app = exports.app = Application(require("./actions"));
+var app = exports.app = require("./actions").app;
 app.configure("etag", "responselog", "error", "notfound", "session", "static");
 app.static(module.resolve("public"));
 
