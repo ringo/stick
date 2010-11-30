fff63a102e6558c25daca1408e9ffdaa14a14eac
Hannes Wallnöfer <hannesw@gmail.com>
2010-11-30 12:28:22 +0100

    Add example for mount and route middleware

---
+++ examples/mount-route/app.js
@@ -0,0 +1,33 @@
+var {Application} = require("stick");
+var {linkTo, urlFor} = require("stick/helpers");
+
+var app = exports.app = Application(),
+    foo = module.resolve("foo"),
+    bar = module.resolve("bar");
+
+app.configure("mount", "route");
+app.mount("/foo", foo);
+app.mount("/bar", bar);
+
+app.get("/", function(req) {
+    return {
+        status: 200,
+        headers: {"Content-Type": "text/html"},
+        body: ["<html><body><h1>Mount/Route middleware demo</h1>" +
+              "<p>This app demos the composition and linking capabilities of the mount and route middleware. " +
+                 "Some links: </p>" +
+                 "<ul>" +
+                     "<li>" + linkTo({app: foo}) + "</li>" +
+                     "<li>" + linkTo({app: bar, name: "hello"}) + "</li>" +
+                     "<li>" + linkTo({app: foo, name: "hello", ext: "world"}) + "</li>" +
+                 "</ul>" +
+              "</body></html>"]
+    }
+});
+
+if (require.main === module) {
+    var Server = require("ringo/httpserver").Server;
+    var server = server || new Server({config: module.id, app: "app"});
+    server.start();
+}
+
---
+++ examples/mount-route/bar.js
@@ -0,0 +1,21 @@
+var {Application} = require("stick");
+var {linkTo, urlFor} = require("stick/helpers");
+
+var app = exports.app = Application(),
+    foo = module.resolve("foo"),
+    home = module.resolve("app");
+
+app.configure("route");
+
+// Define an index route that takes optional name and ext arguments.
+// Link to the other module's index action with the same name and ext.
+app.get("/:name?.:ext?", function(req, name, ext) {
+    return {
+        status: 200,
+        headers: {"Content-Type": "text/html"},
+        body: ["<html><body><h1>Bar</h1><p>This is module <b>'bar'</b> called with <b>" +
+               name + "</b>, <b>" + ext + "</b>. " +
+               "Go to " + linkTo({app: foo, name: name, ext: ext}) +
+               " or back " + linkTo({app: home}, "home") + ".</p></body></html>"]
+    };
+});
