fa6e8df28a3784f9e8e423e14c893286e62afb16
Hannes Wallnöfer <hannesw@gmail.com>
2010-12-04 00:01:01 +0100

    Add some comments to continuation examples

--- examples/continuation/app.js
+++ examples/continuation/app.js
@@ -10,6 +10,8 @@ var {request} = require("ringo/httpclient");
 var app = exports.app = Application();
 app.configure("error", "notfound", "session", "params", "continuation", "route");
 
+// generator action that yields a promise and is resumed when the promise
+// is resolved, yielding a JSGI response
 app.get("/", function (req) {
     var deferred = defer();
     setTimeout(function() {
@@ -21,6 +23,9 @@ app.get("/", function (req) {
     yield htmlResponse(body);
 });
 
+// generator action that fetches 3 URLs in parallel, yields a promise for the
+// requests and is resumed when all requests are finished, returning a JSGI
+// response with the HTTP status codes
 app.get("/http", function(req) {
     var results = yield promises(
         request({url: "http://www.orf.at", promise: true}),
@@ -32,7 +37,7 @@ app.get("/http", function(req) {
     }).join("<br/>"));
 });
 
-// same as index action, but resolve to error
+// same as index action above, but resolve to error
 app.get("/error", function(req) {
     var deferred = defer();
     setTimeout(function() {
@@ -44,6 +49,9 @@ app.get("/error", function(req) {
     yield htmlResponse(body);
 });
 
+// an example for a user/jsgi continuation. The generator is stored
+// in the user's session and yields a response each time the
+// continuation middleware sees a request with the generator's id.
 app.get("/counter", function(req) {
     var counter = 0;
     var c = app.continuation.activate();
@@ -57,6 +65,8 @@ app.get("/counter", function(req) {
     }
 });
 
+// This is a plain old promise based asynchronous action. Does not rely on
+// continuation middleware, just here for reference and comparison.
 app.get("/async", function(req) {
     var deferred = defer();
     setTimeout(function() {
