3cdc6e285b977ef4ac91a77a8f7169cb3a11d262
Hannes Wallnöfer <hannesw@gmail.com>
2010-12-09 17:57:56 +0100

    Remove option parsing from main script to lighten it up

--- skeleton/app/main.js
+++ skeleton/app/main.js
@@ -1,30 +1,26 @@
 
-var system = require("system");
 var {Application} = require("stick");
 var {Server} = require("ringo/httpserver");
-var {Parser} = require("ringo/args");
 var server, app;
 
-export("app", "init", "start", "stop", "destroy", "server");
+export("app");
 
 app = Application();
-app.configure("notfound", "error", "static", "mount");
+app.configure("notfound", "error", "static", "params", "mount");
 app.static(module.resolve("public"));
-app.mount("/", module.resolve("actions"));
+app.mount("/", require("./actions"));
 
 
 // Daemon life-cycle functions.
 
 function init() {
-    var options = parseOptions();
-    server = server || new Server(options);
-}
-
-function start() {
-    server.getDefaultContext().serveApplication({
+    server = server || new Server({
         config: module.id,
         app: "app"
     });
+}
+
+function start() {
     server.start();
 }
 
@@ -34,31 +30,6 @@ function stop() {
 
 function destroy() {
     server.destroy();
-}
-
-function parseOptions() {
-    var script = system.args.shift();
-    var parser = new Parser()
-        .addOption("j", "jetty-config", "PATH", "Jetty configuration file (default: 'config/jetty.xml')")
-        .addOption("H", "host", "ADDRESS", "IP address to bind to (default: 0.0.0.0)")
-        .addOption("p", "port", "PORT", "TCP port to listen on (default: 8080)")
-        .addOption("v", "virtual-host", "VHOST", "Virtual host name (default: undefined)")
-        .addOption("h", "help", null, "Print this message and exit");
-    var options = parser.parse(system.args);
-    if (options.help) {
-        print("Usage:");
-        print("ringo", script, "[OPTIONS]");
-        print("Options:");
-        print(parser.help());
-        system.exit();
-    }
-    if (options.port) {
-        if (isNaN(options.port)) {
-            throw new Error("Invalid value for port: " + options.port);
-        }
-        options.port = +options.port;
-    }
-    return options;
 }
 
 // Script run from command line
