0813716d2bdef3ee15da30bccaa2639cdb9d5153
Jim Cook <github@visualxs.com>
2011-08-01 08:45:55 -0400

    Added a test to show problems with the resolution order of paths containing parameters. Added code to fix the sort order of routes in the route middleware.

--- test/stick_test.js
+++ test/stick_test.js
@@ -1,5 +1,5 @@
 var {Application} = require("../lib/stick");
-var {mount} = require("../lib/middleware")
+var {mount,route} = require("../lib/middleware")
 var assert = require("assert");
 
 
@@ -65,6 +65,82 @@ exports.testMount = function() {
     app = new Application();
     app.configure("mount");
     testMount(app);
+};
+
+exports.testMountSort = function() {
+    var app = new Application();
+    app.configure(mount);
+
+    app.mount("/", function() { return "root" });
+    app.mount("/foo", function() { return "foo" });
+    app.mount("/foo/bar", function() { return "foo/bar" });
+
+    assert.equal(app({headers: {host: "foo.com"}, env: {}, pathInfo: "/"}), "root");
+    assert.equal(app({headers: {host: "foo.com"}, env: {}, pathInfo: "/foo"}), "foo");
+    assert.equal(app({headers: {host: "foo.com"}, env: {}, pathInfo: "/foo/bar"}), "foo/bar");
+
+    try {
+        var response = app({headers: {host: "foo.com"}, env: {}, pathInfo: "/bars"});
+        assert.fail('Expecting unhandled request');
+    } catch(e) { }
+};
+
+/**
+ * Test that makes sure the most 'literal' version of the URL is resolved instead of parameterized
+ * version. ie /foo wins over /:param when url is /foo.
+ */
+exports.testRouteResolution = function() {
+    function testPath(path, expected) {
+        assert.equal(app({method: 'GET', headers: {host: "foo.com"}, env: {}, pathInfo: path}), expected);
+    }
+
+    var app = new Application();
+    app.configure(route);
+
+    app.get("/:param", function(req, p) { return '[' + p + ']' });
+    app.get("/foo", function() { return "foo" });
+    app.get("/bar/:param", function(req, p) { return 'bar/[' + p + ']'});
+    app.get("/bar/foo", function() { return "bar/foo" });
+    app.get("/baz/:param/qux", function(req, p) { return 'baz/[' + p + ']/qux'});
+    app.get("/baz/123/qux", function() { return "baz/123/qux" });
+
+    testPath("/foo", "foo");
+    testPath("/abc", "[abc]");
+    testPath("/bar/foo", "bar/foo");
+    testPath("/bar/abc", "bar/[abc]");
+    testPath("/baz/abc/qux", "baz/[abc]/qux");
+    testPath("/baz/123/qux", "baz/123/qux");
+};
+
+/**
+ * Test that makes sure the most 'literal' version of the URL is resolved instead of parameterized
+ * version. ie /foo wins over /:param when url is /foo.
+ */
+exports.testMountAndRouteResolution = function() {
+    function testPath(path, expected) {
+        assert.equal(mountApp({method: 'GET', headers: {host: "foo.com"}, env: {}, pathInfo: path}), expected);
+    }
+
+    var app = new Application();
+    app.configure(route);
+
+    app.get("/:param", function(req, p) { return '[' + p + ']'});
+    app.get("/foo", function() { return "foo" });
+    app.get("/bar/foo", function() { return "bar/foo" });
+    app.get("/bar/:param", function(req, p) { return 'bar/[' + p + ']'});
+    app.get("/baz/:param/qux", function(req, p) { return 'baz/[' + p + ']/qux'});
+    app.get("/baz/123/qux", function() { return "baz/123/qux" });
+
+    var mountApp = new Application();
+    mountApp.configure(mount);
+    mountApp.mount("/test", app);
+
+    testPath("/test/foo", "foo");
+    testPath("/test/abc", "[abc]");
+    testPath("/test/bar/foo", "bar/foo");
+    testPath("/test/bar/abc", "bar/[abc]");
+    testPath("/test/baz/abc/qux", "baz/[abc]/qux");
+    testPath("/test/baz/123/qux", "baz/123/qux");
 };
 
 if (require.main == module) {
