888d637785ef3e04be0b55162ba62485958310b3
Jim Cook <github@visualxs.com>
2011-08-16 12:45:40 -0400

    Added support for mounting paths using the typical REST style syntax with no trailing slash.

--- test/stick_test.js
+++ test/stick_test.js
@@ -67,6 +67,54 @@ exports.testMount = function() {
     testMount(app);
 };
 
+/**
+* The default behavior of mount.js middleware will issue a 303 redirect if the
+* user enters a mount path which does not end with a slash on a GET request.
+* The redirect returns the browser to the same path, but with a trailing slash.
+* Not very nice for performance or style when using REST urls.
+*/
+exports.testMountRedirect = function() {
+    var response;
+
+    var app = new Application();
+    app.configure(mount);
+
+    app.mount("/", function() { return "root" });
+    app.mount("/foo", function() { return "foo" });
+    app.mount("/foo/bar", function() { return "foo/bar" });
+
+    // These URLs should return a 303 response using the default URL treatment
+    response = app({headers: {}, method: "GET", env: {}, scriptName: "", pathInfo: ""});
+    assert.strictEqual(response.status, 303);
+    assert.strictEqual(response.headers.Location, "/");
+    response = app({headers: {}, method: "GET", env: {}, scriptName: "", pathInfo: "/foo"});
+    assert.strictEqual(response.status, 303);
+    assert.strictEqual(response.headers.Location, "/foo/");
+    response = app({headers: {}, method: "GET", env: {}, scriptName: "", pathInfo: "/foo/bar"});
+    assert.strictEqual(response.status, 303);
+    assert.strictEqual(response.headers.Location, "/foo/bar/");
+};
+
+/**
+* When using the mount command, the developer can choose to use REST-style URLs
+* without a redirect and without a trailing slash on the end of the URL.
+*/
+exports.testMountNoRedirect = function() {
+    var response;
+
+    var app = new Application();
+    app.configure(mount);
+
+    app.mount("/", function() { return "root" }, true);
+    app.mount("/foo", function() { return "foo" }, true);
+    app.mount("/foo/bar", function() { return "foo/bar" }, true);
+
+    // Using REST urls, these requests should return the expected content
+    assert.equal(app({headers: {}, env: {}, method: "GET", pathInfo: ""}), "root");
+    assert.equal(app({headers: {}, env: {}, method: "GET", pathInfo: "/foo"}), "foo");
+    assert.equal(app({headers: {}, env: {}, method: "GET", pathInfo: "/foo/bar"}), "foo/bar");
+};
+
 exports.testMountSort = function() {
     var app = new Application();
     app.configure(mount);
