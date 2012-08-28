2a861c1ba4c8ccb220cc0e9091ebf4b55e014227
Hannes Wallnöfer <hannesw@gmail.com>
2012-08-28 17:17:42 +0200

    Adapt to changed ringo/promise API and ringo/httpclient no longer providing async operation.

--- examples/continuation/app.js
+++ examples/continuation/app.js
@@ -4,7 +4,8 @@
 var {Application} = require("stick");
 var {linkTo, redirectTo} = require("stick/helpers");
 var {html} = require("ringo/jsgi/response");
-var {defer, promises} = require("ringo/promise");
+var {Deferred, PromiseList} = require("ringo/promise");
+var {WorkerPromise} = require("ringo/worker");
 var {setTimeout} = require("ringo/scheduler");
 var {request} = require("ringo/httpclient");
 
@@ -14,7 +15,7 @@ app.configure("error", "notfound", "session", "params", "continuation", "route")
 // generator action that yields a promise and is resumed when the promise
 // is resolved, yielding a JSGI response
 app.get("/", function (req) {
-    var deferred = defer();
+    var deferred = new Deferred();
     // resolve promise 2 seconds from now to "hello world"
     setTimeout(deferred.resolve, 2000, "hello world");
     // yield a promise
@@ -27,10 +28,12 @@ app.get("/", function (req) {
 // requests and is resumed when all requests are finished, returning a JSGI
 // response with the HTTP status codes
 app.get("/http", function(req) {
-    var results = yield promises(
-        request({url: "http://www.orf.at", promise: true}),
-        request({url: "http://localhost:8080/foo", promise: true}),
-        request({url: "http://www.google.at/", promise: true}));
+    // Start three workers with the current module (see onmessage handler below)
+    // that operate as promises.
+    var results = yield new PromiseList(
+        new WorkerPromise(module.id, "http://www.orf.at"),
+        new WorkerPromise(module.id, "http://localhost:8080/foo"),
+        new WorkerPromise(module.id, "http://www.google.at/"));
     yield html(results.map(function(res) {
         return res.error ? "Error: " + res.error.status :
                            "Success: " + res.value.status;
@@ -39,7 +42,7 @@ app.get("/http", function(req) {
 
 // same as index action above, but resolve to error
 app.get("/error", function(req) {
-    var deferred = defer();
+    var deferred = new Deferred();
     // resolve promise 2 seconds from now to an error
     setTimeout(deferred.resolve, 2000, "something went wrong", true);
     // yield a promise
@@ -72,6 +75,11 @@ app.get("/async", function(req) {
     // return a promise
     return deferred.promise;    
 });
+
+// Worker message handler
+function onmessage(e) {
+    e.source.postMessage(request({url: e.data}));
+}
 
 
 // start server if run as main script
