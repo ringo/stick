bf790c6eff6e6832e10a28d80b9bd3915ff67fa1
Philipp Naderer <philipp.naderer@gmail.com>
2016-10-20 17:54:59 +0200

    Advances the config options for static middleware
    * Cache-Control / max-age
    * lastModified header by default
    * setHeaders() function for custom cache headers
    * Security options to prevent serving dotfiles

--- test/middleware/static_test.js
+++ test/middleware/static_test.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const system = require("system");
 const assert = require("assert");
 
@@ -58,10 +59,174 @@ exports.testStaticFile = function() {
    });
    assert.equal(response.headers["Content-Type"], "text/html");
    assert.equal(bodyAsString(response.body, "utf-8"), "<!DOCTYPE html>");
+   assert.equal(response.headers["Cache-Control"], "max-age=0");
+};
+
+exports.testMaxAge = function() {
+   let app = new Application();
+
+   app.configure("static");
+   app.static(module.resolve("./fixtures"), "index.html", "/customStatic", {
+      maxAge: 1000
+   });
+
+   let response = app({
+      method: 'GET',
+      headers: {},
+      env: {},
+      pathInfo: '/customStatic/'
+   });
+   assert.equal(response.headers["Content-Type"], "text/html");
+   assert.equal(bodyAsString(response.body, "utf-8"), "<!DOCTYPE html>");
+   assert.equal(response.headers["Cache-Control"], "max-age=1000");
+
+   app = new Application();
+
+   app.configure("static");
+   app.static(module.resolve("./fixtures"), "index.html", "/customStatic");
+
+   response = app({
+      method: 'GET',
+      headers: {},
+      env: {},
+      pathInfo: '/customStatic/'
+   });
+   assert.equal(response.headers["Content-Type"], "text/html");
+   assert.equal(bodyAsString(response.body, "utf-8"), "<!DOCTYPE html>");
+   assert.equal(response.headers["Cache-Control"], "max-age=0");
+};
+
+exports.testLastModified = function() {
+   var app = new Application();
+   app.configure("static");
+   app.static(module.resolve("./fixtures"), "index.html", "/customStatic", {
+      lastModified: false
+   });
+
+   let response = app({
+      method: 'GET',
+      headers: {},
+      env: {},
+      pathInfo: '/customStatic/'
+   });
+   assert.equal(response.headers["Content-Type"], "text/html");
+   assert.equal(bodyAsString(response.body, "utf-8"), "<!DOCTYPE html>");
+   assert.isUndefined(response.headers["Last-Modified"]);
+
+   app = new Application();
+   app.configure("static");
+   app.static(module.resolve("./fixtures"), "index.html", "/customStatic");
+
+   response = app({
+      method: 'GET',
+      headers: {},
+      env: {},
+      pathInfo: '/customStatic/'
+   });
+   assert.equal(response.headers["Content-Type"], "text/html");
+   assert.equal(bodyAsString(response.body, "utf-8"), "<!DOCTYPE html>");
+   assert.isNotUndefined(response.headers["Last-Modified"]);
+
+   let lastModified = fs.lastModified(fs.join(module.resolve("./fixtures"), "index.html"));
+   let sdf = new java.text.SimpleDateFormat("E, d MMM yyyy HH:mm:ss z", java.util.Locale.US);
+   sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT"));
+   assert.equal(response.headers["Last-Modified"], sdf.format(lastModified));
+};
+
+exports.testSetHeaders = function() {
+   let app = new Application();
+
+   app.configure("static");
+   app.static(module.resolve("./fixtures"), "index.html", "/customStatic", {
+      setHeaders: function() {
+         return {
+            "x-foo": "bar",
+            "x-seestadt-cafe": "united in cycling",
+            "Cache-Control": "max-age=123456"
+         }
+      }
+   });
+
+   let response = app({
+      method: 'GET',
+      headers: {
+         "accept-encoding": "gzip"
+      },
+      env: {},
+      pathInfo: '/customStatic/'
+   });
+
+   assert.equal(response.status, 200);
+   assert.equal(response.headers["Content-Type"], "text/html");
+   assert.equal(response.headers["Content-Encoding"], "gzip");
+   assert.equal(response.headers["Cache-Control"], "max-age=123456");
+   assert.equal(response.headers["x-foo"], "bar");
+   assert.equal(response.headers["x-seestadt-cafe"], "united in cycling");
+};
+
+exports.testDotfiles = function() {
+   let app = new Application();
+
+   app.configure("static");
+   app.static(module.resolve("./fixtures"), "index.html", "/customStatic", {
+      "dotfiles": "allow"
+   });
+
+   let response = app({
+      method: 'GET',
+      headers: {
+         "accept-encoding": "gzip"
+      },
+      env: {},
+      pathInfo: '/customStatic/.DotfileExample'
+   });
+
+   assert.equal(response.status, 200);
+   assert.equal(response.headers["Content-Type"], "text/plain");
+
+   // DENY
+   app = new Application();
+
+   app.configure("static");
+   app.static(module.resolve("./fixtures"), "index.html", "/customStatic", {
+      "dotfiles": "deny"
+   });
+
+   response = app({
+      method: 'GET',
+      headers: {
+         "accept-encoding": "gzip"
+      },
+      env: {},
+      pathInfo: '/customStatic/.DotfileExample'
+   });
+
+   assert.equal(response.status, 403);
+   assert.equal(response.headers["content-type"], "text/plain; charset=utf-8");
+
+   // IGNORE
+   app = new Application();
+
+   app.configure("static");
+   app.static(module.resolve("./fixtures"), "index.html", "/customStatic", {
+      "dotfiles": "IgNoRe"
+   });
+
+   response = app({
+      method: 'GET',
+      headers: {
+         "accept-encoding": "gzip"
+      },
+      env: {},
+      pathInfo: '/customStatic/.DotfileExample'
+   });
+
+   assert.equal(response.status, 404);
+   assert.equal(response.headers["content-type"], "text/plain; charset=utf-8");
 };
 
 exports.testPrecompressedStaticFile = function() {
-   var app = new Application();
+   let app = new Application();
 
    app.configure("static");
    app.static(module.resolve("./fixtures"), "index.html", "/customStatic", {});
@@ -78,6 +243,7 @@ exports.testPrecompressedStaticFile = function() {
    assert.equal(response.status, 200);
    assert.equal(response.headers["Content-Type"], "text/html");
    assert.equal(response.headers["Content-Encoding"], "gzip");
+   assert.equal(response.headers["Cache-Control"], "max-age=0");
 
    response = app({
       method: 'GET',
@@ -91,6 +257,7 @@ exports.testPrecompressedStaticFile = function() {
    assert.equal(response.status, 200);
    assert.equal(response.headers["Content-Type"], "text/html");
    assert.equal(response.headers["Content-Encoding"], "gzip");
+   assert.equal(response.headers["Cache-Control"], "max-age=0");
 
    const ba = new ByteArray([0x1F, 0x8B, 0x08, 0x08, 0x81, 0xF1, 0xD6, 0x56, 0x00, 0x03, 0x69, 0x6E, 0x64, 0x65, 0x78,
       0x2E, 0x68, 0x74, 0x6D, 0x6C, 0x00, 0xB3, 0x51, 0x74, 0xF1, 0x77, 0x0E, 0x89, 0x0C, 0x70,
@@ -105,6 +272,7 @@ exports.testPrecompressedStaticFile = function() {
    });
    assert.equal(response.headers["Content-Type"], "text/html");
    assert.equal(bodyAsString(response.body, "utf-8"), "<!DOCTYPE html><foo></foo>");
+   assert.equal(response.headers["Cache-Control"], "max-age=0");
 };
 
 exports.testDeactivatedPrecompression = function() {
@@ -123,6 +291,7 @@ exports.testDeactivatedPrecompression = function() {
    });
    assert.equal(response.headers["Content-Type"], "text/html");
    assert.equal(bodyAsString(response.body, "utf-8"), "<!DOCTYPE html>");
+   assert.equal(response.headers["Cache-Control"], "max-age=0");
 
    response = app({
       method: 'GET',
@@ -132,6 +301,7 @@ exports.testDeactivatedPrecompression = function() {
    });
    assert.equal(response.headers["Content-Type"], "text/html");
    assert.equal(bodyAsString(response.body, "utf-8"), "<!DOCTYPE html><foo></foo>");
+   assert.equal(response.headers["Cache-Control"], "max-age=0");
 };
 
 if (require.main === module) {
