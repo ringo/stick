4ad42e550b6719625ab6d5c5c29bd72f66b1d145
Philipp Naderer-Puiu <philipp@naderer-puiu.at>
2021-08-03 14:45:27 +0200

    Changes for non-global binary module

--- test/middleware/gzip_test.js
+++ test/middleware/gzip_test.js
@@ -1,15 +1,12 @@
-const system = require("system");
+const binary = require("binary");
 const assert = require("assert");
-const base64 = require("ringo/base64");
 const {text} = require("ringo/jsgi/response");
-
-let {ByteArray} = require("binary");
 
 const {Application} = require("../../lib/stick");
 const {gzip, route} = require("../../lib/middleware");
 
 const TEST_STRING = "a simple string".repeat(1000);
-const BIN_RESULT = new ByteArray([
+const BIN_RESULT = new binary.ByteArray([
     31, 139, 8, 0, 0, 0, 0, 0, 0, 0, 237, 199, 65, 9, 0, 32, 16, 0, 176, 42, 87, 205, 135, 200, 129, 138, 168, 253,
     177, 128, 17, 182, 223, 74, 156, 28, 171, 215, 56, 119, 231, 108, 69, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85,
     85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 245, 215, 7, 179, 94, 205, 158, 152, 58, 0, 0
@@ -19,8 +16,8 @@ const bodyAsByteArray = function(body) {
    if (body && typeof body.forEach == "function") {
       var output = new java.io.ByteArrayOutputStream();
       var writer = function(part) {
-         if (!(part instanceof Binary)) {
-            part = toByteString(part, charset);
+         if (!(part instanceof binary.Binary)) {
+            part = binary.toByteString(part, charset);
          }
          output.write(part);
       };
@@ -29,7 +26,7 @@ const bodyAsByteArray = function(body) {
          body.close(writer);
       }
 
-      return ByteArray.wrap(output.toByteArray());
+      return binary.ByteArray.wrap(output.toByteArray());
    } else {
       throw new Error("Response body doesn't implement forEach: " + body);
    }
