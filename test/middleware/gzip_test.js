f8574a8109baefaedf2eed4e9d1ae78f48e29a3e
Robert Gaggl <robert@nomatic.org>
2021-11-06 14:30:22 +0100

    fix: gzip middleware threw an exception when the gzip buffer size exceeded 1024 bytes

--- test/middleware/gzip_test.js
+++ test/middleware/gzip_test.js
@@ -1,21 +1,38 @@
 const binary = require("binary");
 const assert = require("assert");
 const {text} = require("ringo/jsgi/response");
+const {Random, Arrays} = java.util;
+const {GZIPOutputStream} = java.util.zip;
+const {MemoryStream} = require("io");
+const {ByteArrayOutputStream} = java.io;
 
 const {Application} = require("../../lib/stick");
 const {gzip, route} = require("../../lib/middleware");
 
-const TEST_STRING = "a simple string".repeat(1000);
-const BIN_RESULT = new binary.ByteArray([
-    31, 139, 8, 0, 0, 0, 0, 0, 0, 0, 237, 199, 65, 9, 0, 32, 16, 0, 176, 42, 87, 205, 135, 200, 129, 138, 168, 253,
-    177, 128, 17, 182, 223, 74, 156, 28, 171, 215, 56, 119, 231, 108, 69, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85,
-    85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 85, 245, 215, 7, 179, 94, 205, 158, 152, 58, 0, 0
-]);
+const compress = (bytes) => {
+    let inStream, outStream, byteArrayOutputStream;
+    try {
+        inStream = new MemoryStream(bytes);
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        outStream = new GZIPOutputStream(byteArrayOutputStream);
+        inStream.copy(outStream);
+        outStream.finish();
+        return binary.ByteArray.wrap(byteArrayOutputStream.toByteArray());
+    } finally {
+        outStream && outStream.close();
+        inStream && inStream.close();
+    }
+};
+
+const testBytes = new binary.ByteArray(50000);
+(new Random()).nextBytes(testBytes.unwrap());
+const TEST_STRING = testBytes.decodeToString();
+const BIN_RESULT = compress(binary.toByteArray(TEST_STRING));
 
 const bodyAsByteArray = function(body) {
    if (body && typeof body.forEach == "function") {
-      var output = new java.io.ByteArrayOutputStream();
-      var writer = function(part) {
+      const output = new ByteArrayOutputStream();
+      const writer = function(part) {
          if (!(part instanceof binary.Binary)) {
             part = binary.toByteString(part, charset);
          }
@@ -25,7 +42,6 @@ const bodyAsByteArray = function(body) {
       if (typeof body.close == "function") {
          body.close(writer);
       }
-
       return binary.ByteArray.wrap(output.toByteArray());
    } else {
       throw new Error("Response body doesn't implement forEach: " + body);
@@ -59,7 +75,7 @@ exports.testGzip = function() {
 
     assert.equal(res.status, 200);
     assert.isTrue(
-        java.util.Arrays.equals(
+        Arrays.equals(
             bodyAsByteArray(res.body).unwrap(),
             BIN_RESULT.unwrap()
         )
