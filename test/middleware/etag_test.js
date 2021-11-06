675fe5ac4be70fad05e78e95c247bb8f23301fcd
Robert Gaggl <robert@nomatic.org>
2021-11-06 12:15:27 +0100

    fix: etag middleware corrupted binary stream responses
    (see ringo/ringojs#439) to construct the etag md5 digest it copies the response body into an array of byte arrays/byte strings, but `Stream.forEach()` re-uses a single binary buffer [1], so if the streamed response exceeds 8192 bytes and thus be split up into multiple buffers the body created by etag middleware would essentially consist of different byte arrays all containing the same bytes. the fix is to use `Binary.prototype.slice()` to copy the body part - this is expensive and effectively thwarts the use of `response.stream()`, but it's what the etag middleware did before #409 too, and at least it doesn't corrupt binary responses anymore.
    
    [1] https://github.com/ringo/ringojs/blob/master/modules/io.js#L48

--- test/middleware/etag_test.js
+++ test/middleware/etag_test.js
@@ -1,5 +1,9 @@
 const assert = require("assert");
 const {Headers} = require("ringo/utils/http");
+const fs = require("fs");
+const {Arrays} = java.util;
+const response = require("ringo/jsgi/response");
+const {MemoryStream} = require("io");
 
 const {Application} = require("../../lib/stick");
 
@@ -31,6 +35,27 @@ exports.testBasicMD5 = function() {
     assert.equal(headers.get("content-type"), "text/html");
     assert.equal(headers.get("cache-control"), "max-age=0");
     assert.equal(headers.get("etag"), "1234567890");
+};
+
+exports.testBinaryStreamResponse = function() {
+    const file = module.resolve("./fixtures/ringo-drums.png");
+    const app = new Application();
+    app.configure("etag", "route");
+    app.get("/", function() {
+        return response.stream(fs.open(file, {binary: true}), "image/png");
+    });
+    const result = app({
+        method: 'GET',
+        headers: {},
+        env: {},
+        pathInfo: '/'
+    });
+    const headers = Headers(result.headers);
+    assert.equal(headers.get("content-type"), "image/png");
+    assert.equal(headers.get("etag"), "0C15DE3FBC9D7A7B936F98147CD7FDCB");
+    const body = new MemoryStream();
+    result.body.forEach(part => body.write(part));
+    assert.isTrue(Arrays.equals(body.content, fs.read(file, {binary: true})));
 };
 
 if (require.main === module) {
